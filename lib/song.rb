6da88ece9c6d2d0938d82928b5d8846f6f73fef4
Romena Murgor <romenamurgor@gmail.com>
2020-05-13 02:52:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,51 +1,44 @@
-class Song
+class Song 
   attr_accessor :name, :artist, :genre
-
+  
   @@count = 0
-  @@artists = []
   @@genres = []
-
+  @@artists = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
+    @@count += 1
     @@genres << genre
     @@artists << artist
   end
-
+  
   def self.count
     @@count
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
-
+  end 
+  
   def self.genres
     @@genres.uniq
   end
-
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
+   
+   def self.artists 
+     @@artists.uniq
+   end
+   
+   def self.genre_count 
+     count_of_genres = {}
+     @@genres.each do |genre_name|
+       count_of_genres[genre_name] = @@genres.count(genre_name)
       end
-    end
-    genre_count
-  end
+      count_of_genres
+   end
+   
+   def self.artist_count
+     count_of_artists = {}
+     @@artists.each do |artist_name|
+       count_of_artists[artist_name] = @@artists.count(artist_name)
+     end
+     count_of_artists
+   end
+end
